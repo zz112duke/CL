c85b2111cf7907c72d61303e8c9c4b94b62af918
Ziwei Zhang <44096307+zz112duke@users.noreply.github.com>
2019-11-01 15:40:06 -0400

    Add files via upload

--- other/matrix.py
+++ other/matrix.py
@@ -17,7 +17,7 @@ os.chdir(_thisDir)
 binDir = _thisDir + os.sep + u'bin'
 
 # Store info about the experiment session
-expName = 'CCL'  # from the Builder filename that created this script
+expName = 'CCL_Post'  # from the Builder filename that created this script
 expInfo = {'participant':'','session':'001'}
 dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
 if dlg.OK == False:
@@ -39,7 +39,7 @@ logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a f
 
 # Setup the Window
 win = visual.Window(
-    size=(1024, 768), fullscr=False, allowGUI=False,
+    size=(1024, 768), fullscr=True, allowGUI=False,
     monitor='testMonitor', color=[1,1,1], useFBO=True)
 
 ##########Timer##########
@@ -49,54 +49,6 @@ Practice_2_Clock = core.Clock()
 Task_1_Clock = core.Clock()
 Task_2_Clock = core.Clock()
 
-##########Version##########
-# version 1 [non,con] version 2 [con,non]
-version = random.choice([1,2])
-#print (version)
-
-
-##########Stimuli##########
-Imagelist1 = list(os.listdir(_thisDir + '/Set1'))
-Imagelist1 = ["Set1/" + i for i in Imagelist1]
-
-Imagelist2 = list(os.listdir(_thisDir + '/Set2'))
-Imagelist2 = ["Set2/" + i for i in Imagelist2]
-
-Practicelist = list(os.listdir(_thisDir + '/Practice'))
-Practicelist = ["Practice/" + i for i in Practicelist]
-#print(Practicelist)
-
-Image = visual.ImageStim(win=win, name='Image', 
-    image= _thisDir + '/Set1/CK_f_01.jpg', mask=None,
-    ori=0, pos=(0, 0), opacity=1, texRes=128, depth=0,
-    size=(0.75, 1), interpolate = True)
-
-stroop_text =visual.TextStim(win=win, name='stroop_text',
-    text='default',font=u'Arial',
-    pos=(0, 0), height=0.2, wrapWidth=None, ori=0, 
-    color=u'red', colorSpace='rgb', opacity=1,
-    depth=0)
-
-Blank = visual.TextStim(win=win, name='blank', text='h', 
-    font=u'Arial', pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
-    color=u'black', colorSpace='rgb', opacity=0, depth=0.0)
-
-Correct =visual.TextStim(win=win, name='Correct',
-    text='Correct',font=u'Arial',
-    pos=(0, -0.75), height=0.2, wrapWidth=None, ori=0, 
-    color=u'red', colorSpace='rgb', opacity=1,
-    depth=0)
-
-Incorrect =visual.TextStim(win=win, name='Incorrect',
-    text='Incorrect',font=u'Arial',
-    pos=(0, -0.75), height=0.2, wrapWidth=None, ori=0, 
-    color=u'red', colorSpace='rgb', opacity=1,
-    depth=0)
-
-Post_Q1 = visual.TextStim(win=win, name='blank', text='Have you noticed that some face images were presented with different frequencies \ (i.e. some showed up more times than the others)? \ Press' + 'y' + 'if you noticed and' + 'n' + 'if you did not', 
-    font=u'Arial', pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
-    color=u'black', colorSpace='rgb', opacity=1, depth=0.0)
-
 Instr_Post = visual.TextStim(win=win, name='blank', text='Now you will be presented with images that you have seen in the main experiment. You will be asked to approxiamte the relative frequency that the images were presented. If your answer is the left image, press left; if it is the right image, press right. Press the space bar to contiune. ', 
     font=u'Arial', pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color=u'black', colorSpace='rgb', opacity=1, depth=0.0)
@@ -105,7 +57,7 @@ stim_image_right = visual.ImageStim(win=win, name='stim_image_right',
     image= _thisDir + '/Set1/CK_f_01.jpg', mask=None,
     ori=0, pos=(0.4, -0.2), opacity=1, texRes=128, depth=0,
     size=(0.5, 0.67), interpolate = True)
-
+    
 stim_image_left = visual.ImageStim(win=win, name='stim_image_left', 
     image= _thisDir + '/Set1/CK_f_01.jpg', mask=None,
     ori=0, pos=(-0.4, -0.2), opacity=1, texRes=128, depth=0,
@@ -117,114 +69,9 @@ PostFC =visual.TextStim(win=win, name='Post_Q2',
     color=u'red', colorSpace='rgb', opacity=1,
     depth=0)
 
-Ending = visual.TextStim(win=win, name='Instr_1', color='black',
-    text='Thank you for participating in this study. Press the spacebar to quit and call over the researcher for a post-task.')
 
-###Indexing Image###
-# index the images for high, medium and low frequencies for later selection
-#indices_all = (np.random.choice(len(Imagelist), 104, replace=False)).tolist()
-indices_all = list(range(104))
-indices_low = indices_all[:80]
-indices_medium = indices_all[80:96]
-indices_high = indices_all[96:104]
-
-# select corresponding images
-random.shuffle(Imagelist1)
-stim_image_high1 = [Imagelist1[i] for i in indices_high]*10
-stim_image_medium1 = [Imagelist1[i] for i in indices_medium]*5
-stim_image_low1 = [Imagelist1[i] for i in indices_low]
-stim_image1 = Practicelist*3 + stim_image_high1 +stim_image_medium1 + stim_image_low1
-
-random.shuffle(Imagelist2)
-stim_image_high2 = [Imagelist2[i] for i in indices_high]*10
-stim_image_medium2 = [Imagelist2[i] for i in indices_medium]*5
-stim_image_low2 = [Imagelist2[i] for i in indices_low]
-stim_image2 = Practicelist*3 + stim_image_high2 +stim_image_medium2 + stim_image_low2
-#print (stim_image2)
-##########Timing##########
-trials = 240
-duration = 1.0
-ITI_min = 800.0
-ITI_max = 2000.0
-
-ITI = []
-#ITI & duration
-for i in range(252):
-    ITI.append(random.randint(ITI_min, ITI_max)/1000)
-
-duration = [duration]*252
-
-
-##########Frequency##########
-frequency = ['N/A']*12 + ['high']*80 + ['medium']*80 + ['low']*80
-#frequency_practice = ['N/A']*12
-
-##########Congruency##########
-congruency_con = ['con']*6 + ['incon']*6 + ['con']*40 + ['incon']*40 + ['con']*40 + ['incon']*40 + ['con']*40 + ['incon']*40
-#congruency_con_practice = ['con']*6 + ['incon']*6
-congruency_non = ['N/A']*252
-
-
-image_set = [stim_image1,stim_image2]
-random.shuffle(image_set)
-
-##########corrAns##########
-# S-R mapping is different from exp version so needs a new number generator #
-# 0 --> female w, male o; 1 --> female o, male w
-Ans_version = random.choice([0,1])
-
-corrAns1 = []
-corrAns2 = []
-
-if Ans_version==0:
-    SR = ['w','o']
-    for i in image_set[0]:
-        if ('m' or 'M') in i:
-            corrAns1.append(SR[1])
-        else:
-            corrAns1.append(SR[0])
-    for i in image_set[1]:
-        if ('m' or 'M') in i:
-            corrAns2.append(SR[1])
-        else:
-            corrAns2.append(SR[0])
-else:
-    SR = ['o','w']
-    for i in image_set[0]:
-        if ('m' or 'M') in i:
-            corrAns1.append(SR[1])
-        else:
-            corrAns1.append(SR[0])
-    for i in image_set[1]:
-        if ('m' or 'M') in i:
-            corrAns2.append(SR[1])
-        else:
-            corrAns2.append(SR[0])
-
-
-
-##########Exp Matrix##########
-#Turn Exp Matrix into df to randomize rows
-expmatrix_non = [image_set[0], frequency, congruency_non, corrAns1, duration, ITI]
-expmatrix_non = pd.DataFrame(expmatrix_non)
-expmatrix_non = expmatrix_non.transpose()
-expmatrix_non.columns = ['stim_image','Frequency','Congruency','corrAns','Duration','ITI']
-expmatrix_non_exp = expmatrix_non[12:252].sample(frac=1).reset_index(drop=True)
-expmatrix_non_prac = expmatrix_non[0:12].sample(frac=1).reset_index(drop=True)
-
-expmatrix_con = [image_set[1], frequency, congruency_con, corrAns2, duration, ITI]
-expmatrix_con = pd.DataFrame(expmatrix_con)
-expmatrix_con = expmatrix_con.transpose()
-expmatrix_con.columns = ['stim_image','Frequency','Congruency','corrAns','Duration','ITI']
-expmatrix_con_exp = expmatrix_con[12:252].sample(frac=1).reset_index(drop=True)
-expmatrix_con_prac = expmatrix_non[0:12].sample(frac=1).reset_index(drop=True)
-
-
-if version == 1:
-    expmatrix = pd.concat([expmatrix_non_prac , expmatrix_non_exp, expmatrix_con_prac, expmatrix_con_exp],ignore_index=True)
-else:
-    expmatrix = pd.concat([expmatrix_con_prac, expmatrix_con_exp, expmatrix_non_prac , expmatrix_non_exp],ignore_index=True)
-
+##--------------Read the Experimental Matrix---------------##
+expmatrix = pd.read_csv('expmatrix.csv', index_col = [0])
 
 ##--------------Post Forced Choice Matrix---------------##
 # Generate a matrix with F1 vs. F2, where F1 column has 40 'Low' and F2 has 20 'Medium' and 20 'High'. 
@@ -242,19 +89,17 @@ random.shuffle(Question)
 Position = ['L'] * 20 + ['R'] * 20
 random.shuffle(Position)
 
-low_non = expmatrix.loc[(expmatrix.Congruency == 'N/A') & (expmatrix.Frequency == 'low')].sample(n=20)[['stim_image','Congruency']]
-low_con = expmatrix.loc[(expmatrix.Congruency != 'N/A') & (expmatrix.Frequency == 'low')].sample(n=20)[['stim_image','Congruency']]
+low_non = expmatrix.loc[(expmatrix.Congruency == 'None') & (expmatrix.Frequency == 'low')].sample(n=20)[['stim_image','Congruency']]
+low_con = expmatrix.loc[(expmatrix.Congruency != 'None') & (expmatrix.Frequency == 'low')].sample(n=20)[['stim_image','Congruency']]
 
-medium_non = expmatrix.loc[(expmatrix.Congruency == 'N/A') & (expmatrix.Frequency == 'medium')].sample(n=10)[['stim_image','Congruency']]
-medium_con = expmatrix.loc[(expmatrix.Congruency != 'N/A') & (expmatrix.Frequency == 'medium')].sample(n=10)[['stim_image','Congruency']]
+medium_non = expmatrix.loc[(expmatrix.Congruency == 'None') & (expmatrix.Frequency == 'medium')].sample(n=10)[['stim_image','Congruency']]
+medium_con = expmatrix.loc[(expmatrix.Congruency != 'None') & (expmatrix.Frequency == 'medium')].sample(n=10)[['stim_image','Congruency']]
 
-high_non = expmatrix.loc[(expmatrix.Congruency == 'N/A') & (expmatrix.Frequency == 'high')].sample(n=10)[['stim_image','Congruency']]
-high_con = expmatrix.loc[(expmatrix.Congruency != 'N/A') & (expmatrix.Frequency == 'high')].sample(n=10)[['stim_image','Congruency']]
+high_non = expmatrix.loc[(expmatrix.Congruency == 'None') & (expmatrix.Frequency == 'high')].sample(n=10)[['stim_image','Congruency']]
+high_con = expmatrix.loc[(expmatrix.Congruency != 'None') & (expmatrix.Frequency == 'high')].sample(n=10)[['stim_image','Congruency']]
 
 F1_path = pd.concat([low_non,low_con],ignore_index=True)
-print(F1_path)
 F2_path = pd.concat([medium_non, high_non, medium_con, high_con],ignore_index=True)
-print(F2_path)
 corrAns = ['N/A']*40
 
 postmatrix = [F1, F2, Position, Question,corrAns]
